17b9b5d679dddfa9c7da19f56a86fa424316fe78
Srimanyuacharyah <srimanyuacharya@gmail.com>
2026-02-13 14:49:54 +0530

    comit

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
 from db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    address = Column(String)
+    phone = Column(String)
+    alt_phone = Column(String)
+    isprime=Column(Boolean)
